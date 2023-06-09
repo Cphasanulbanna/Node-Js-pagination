69bf6715e5a9bfa5c206a8943d63d6f883f808ad
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-09 07:51:07 +0530

    configured db

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+
+const productRouter = require("./routes/productRouter");
+const { connectDb } = require("./config/db");
+require("dotenv");
+
+connectDb();
 const app = express();
 
 const PORT = 5005;
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/products/", productRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
